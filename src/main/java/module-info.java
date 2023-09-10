c53a308efef2f5ff507aa0c4d64aa22486265e74
CypixDev <cypixzockt@gmail.com>
2023-09-10 21:11:27 +0200

    fixed modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module de.standaloendmx.standalonedmxcontrollpro {
 
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
 
 
     opens de.standaloendmx.standalonedmxcontrolpro to javafx.fxml;
