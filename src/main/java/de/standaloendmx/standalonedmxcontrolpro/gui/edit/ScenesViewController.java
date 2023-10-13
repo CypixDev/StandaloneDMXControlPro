d29ec3bff8294c2a1ecefb1fa3ea6ed1a8b2d2ee
CypixDev <cypixzockt@gmail.com>
2023-10-13 22:03:22 +0200

    new progress

--- src/main/java/de/standaloendmx/standalonedmxcontrolpro/gui/edit/ScenesViewController.java
+++ src/main/java/de/standaloendmx/standalonedmxcontrolpro/gui/edit/ScenesViewController.java
@@ -1,37 +1,22 @@
 package de.standaloendmx.standalonedmxcontrolpro.gui.edit;
 
 import de.standaloendmx.standalonedmxcontrolpro.gui.edit.scene.MyScene;
-import de.standaloendmx.standalonedmxcontrolpro.gui.edit.scene.SceneTableCell;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ScenesViewController implements Initializable {
 
-    @FXML
-    private TableView<MyScene> tvScenes;
 
-    private ObservableList<MyScene> tvData;
+    @FXML
+    private HBox hBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        tvData = FXCollections.observableArrayList();
-
-
-        TableColumn<MyScene, Void> column = new TableColumn<>("Custom Column");
-        column.setCellFactory(param -> new SceneTableCell());
-
-        tvScenes.getColumns().add(column);
-
-        tvData.add(new MyScene());
-        tvScenes.setItems(tvData);
+        hBox.getChildren().add(new MyScene());
 
     }
 }
--- src/main/java/de/standaloendmx/standalonedmxcontrolpro/gui/edit/scene/MyScene.java
+++ src/main/java/de/standaloendmx/standalonedmxcontrolpro/gui/edit/scene/MyScene.java
@@ -1,4 +1,45 @@
 package de.standaloendmx.standalonedmxcontrolpro.gui.edit.scene;
 
-public class MyScene {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MyScene extends VBox implements Initializable {
+
+    private Logger logger = LogManager.getLogger(MyScene.class);
+
+    @FXML
+    private Label name;
+
+    @FXML
+    private Label time;
+
+    @FXML
+    private Label repeat;
+
+    public MyScene() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/edit/scene/MySceneView.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            logger.error(exception);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
