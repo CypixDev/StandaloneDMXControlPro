0cb67b9c588b124702e8b80663dfbe84eb1b56a0
CypixDev <cypixzockt@gmail.com>
2023-10-17 00:21:11 +0200

    properties upgrade

---
+++ src/main/java/de/standaloendmx/standalonedmxcontrolpro/gui/edit/properties/SettingsController.java
@@ -0,0 +1,19 @@
+package de.standaloendmx.standalonedmxcontrolpro.gui.edit.properties;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SettingsController implements Initializable {
+
+    @FXML
+    private Button btnRemote;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
