ed84f6e5768208df7521afdda6d2e82b708fb390
CypixDev <cypixzockt@gmail.com>
2023-11-05 02:50:16 +0100

    made fixtures selectable

--- src/main/java/de/standaloendmx/standalonedmxcontrolpro/gui/bottombar/fader/FixtureSelectViewController.java
+++ src/main/java/de/standaloendmx/standalonedmxcontrolpro/gui/bottombar/fader/FixtureSelectViewController.java
@@ -1,7 +1,9 @@
 package de.standaloendmx.standalonedmxcontrolpro.gui.bottombar.fader;
 
+import de.standaloendmx.standalonedmxcontrolpro.gui.edit.ScenesViewController;
 import de.standaloendmx.standalonedmxcontrolpro.main.StandaloneDMXControlPro;
 import de.standaloendmx.standalonedmxcontrolpro.patch.PatchFixture;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,16 +18,31 @@ public class FixtureSelectViewController implements Initializable {
     @FXML
     private FlowPane flowPane;
 
+
+    private ObservableList<SelectableFixture> fixtures;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-/*        flowPane.widthProperty().addListener((observable, oldValue, newValue) -> {
-            double newWrapLength = newValue.doubleValue();
-            flowPane.setPrefWrapLength(newWrapLength);
-        });*/
+        fixtures = FXCollections.observableArrayList();
+
 
         for (PatchFixture patch : StandaloneDMXControlPro.instance.getPatchManager().getPatches()) {
-            flowPane.getChildren().add(new SelectableFixture(patch));
+            SelectableFixture fx = new SelectableFixture(patch);
+            flowPane.getChildren().add(fx);
+            fixtures.add(fx);
         }
 
+        for (SelectableFixture fixture : fixtures) {
+            fixture.getStyleClass().add("not_selected");
+            fixture.setOnMouseClicked(event -> {
+                if(fixture.getStyleClass().contains("selected")){
+                    fixture.getStyleClass().remove("selected");
+                    fixture.getStyleClass().add("not_selected");
+                }else {
+                    fixture.getStyleClass().remove("not_selected");
+                    fixture.getStyleClass().add("selected");
+                }
+            });
+        }
     }
 }
