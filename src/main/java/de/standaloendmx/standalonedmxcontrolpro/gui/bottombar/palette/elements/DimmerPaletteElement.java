491fa9fd254d7cef55e017990c97ac5efac1d896
CypixDev <cypixzockt@gmail.com>
2023-12-12 00:50:33 +0100

    Add MyFader to DimmerPaletteElement.

--- src/main/java/de/standaloendmx/standalonedmxcontrolpro/gui/bottombar/palette/elements/DimmerPaletteElement.java
+++ src/main/java/de/standaloendmx/standalonedmxcontrolpro/gui/bottombar/palette/elements/DimmerPaletteElement.java
@@ -1,8 +1,13 @@
 package de.standaloendmx.standalonedmxcontrolpro.gui.bottombar.palette.elements;
 
+import de.standaloendmx.standalonedmxcontrolpro.gui.bottombar.fader.MyFader;
+
 public class DimmerPaletteElement extends PaletteElementViewController {
 
     public DimmerPaletteElement() {
+
+        content.getChildren().add(new MyFader(2));
+
         label.setText("Dimmer");
     }
 }
